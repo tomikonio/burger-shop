f917b1ca17889123c63e7f624e3bf050c5f6dab6
Igor <igrr69@gmail.com>
2018-11-01 10:57:27 +0200

    Add side-drawer
    not mobile freindly yet

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
 
 const Layout = props => (
   <React.Fragment>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </React.Fragment>
 );
