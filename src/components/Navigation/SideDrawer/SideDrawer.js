dfede06a806458bc0777c782aaf56c9434d652e8
Igor <igrr69@gmail.com>
2018-11-01 16:28:42 +0200

    Add add a backdrop to the sidedrawer
    close sidedrawer when backdrop is clicked

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.module.css';
 
 const SideDrawer = props => {
+  const attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open === true) {
+    attachedClasses.pop();
+    attachedClasses.push(classes.Open);
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Fragment>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Fragment>
   );
 };
 
