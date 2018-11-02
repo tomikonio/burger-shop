e3a3bcc8dcec7f0192251c82253eb11a9bf1092d
Igor <igrr69@gmail.com>
2018-11-02 10:05:22 +0200

    Change Modal component into a class for performace improvment
    Add shouldComponentUpdate method to modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import Backdrop from '../Backdrop/Backdrop';
-import classes from './Modal.module.css';
+import Backdrop from "../Backdrop/Backdrop";
+import classes from "./Modal.module.css";
 
-const Modal = props => (
-  <Fragment>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Fragment>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default Modal;
